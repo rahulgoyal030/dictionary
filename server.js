50b8331bb5e60d098430eb5e9203682ead0c9a79
rahulgoya030 <goyal.rahul030@gmail.com>
2015-08-24 23:55:43 +0530

    updation

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express= require('express');
 var bodyParser= require('body-parser');
+var request= require('request');
 var app= express();
 
 app.listen(3000, function  () {
 	console.log(" server on")
 });
 
-app.use(express.static());
-app.set(' view engine', 'ejs');
+app.use(express.static('public'));
+
+app.set('view engine','ejs');
 app.set('views' , __dirname +'/views' )
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -15,8 +17,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
 
-//res.sendfile( __dirname + '/index.html');
-
-res.render(index);
+res.render('index');
 
 });
+
+var dictUrl = "https://letsventure.0x10.info/api/dictionary.php?type=json&query=d";
+
+app.get('/search',function  (req,res) {
+	
+	console.log(req.body);
+	 request.get( dictUrl , function (error, response, body) {
+ 		 if (!error && response.statusCode == 200) {
+  					 // console.log(response);
+
+  					// for(x in body)
+  					// {
+  					// 	console.log(x[0].word);
+  					// }
+  					var data= JSON.parse(body);
+  					console.log(data[3].word);
+ 				 }
+		})
+	 res.render('index');
+
+});
+
+
