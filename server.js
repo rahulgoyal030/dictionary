2a5d29d2f580c0677f0c3a4a6be58d3a6168907f
rahulgoya030 <goyal.rahul030@gmail.com>
2015-08-25 00:17:05 +0530

    setting api

--- server.js
+++ server.js
@@ -21,21 +21,26 @@ res.render('index');
 
 });
 
-var dictUrl = "https://letsventure.0x10.info/api/dictionary.php?type=json&query=d";
+
 
 app.get('/search',function  (req,res) {
 	
-	console.log(req.body);
+	console.log(req.url, req.params , req.query.word[0]);
+	var word= req.query.word[0];
+	var dictUrl = "https://letsventure.0x10.info/api/dictionary.php?type=json&query="+word;  // in query put    the request query
+
+
 	 request.get( dictUrl , function (error, response, body) {
  		 if (!error && response.statusCode == 200) {
-  					 // console.log(response);
+  					  //console.log(body);
 
   					// for(x in body)
   					// {
   					// 	console.log(x[0].word);
   					// }
-  					var data= JSON.parse(body);
-  					console.log(data[3].word);
+  				
+  				var data= JSON.parse(body);
+  				console.log(data[3].word);
  				 }
 		})
 	 res.render('index');
